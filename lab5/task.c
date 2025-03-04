a71cdb9668f177d09aa7e6842d704c54e8fd8eba
DvornikovAA404 <advornikov@gmail.com>
2025-03-05 00:01:49 +0700

    InsertSort

--- lab5/task.c
+++ lab5/task.c
@@ -26,16 +26,71 @@ void FillRand(int Arr[], int N){
     }
 }
 
+void ShakerSort(int Arr[], int N, int *Cf, int *Mf) {
+    *Cf = 0;
+    *Mf = 0;
+    int L = 0, R = N - 1, k = N - 1; // Ð Â˜Ð Ð…Ð Ò‘Ð ÂµÐ Ñ”Ð¡ÐƒÐ Â°Ð¡â€ Ð Ñ‘Ð¡Ð Ð¡Ðƒ 0
+
+    do {
+        // Ð ÑŸÐ¡Ð‚Ð Ñ•Ð¡â€¦Ð Ñ•Ð Ò‘ Ð¡ÐƒÐ Ñ—Ð¡Ð‚Ð Â°Ð Ð†Ð Â° Ð Ð…Ð Â°Ð Â»Ð ÂµÐ Ð†Ð Ñ•
+        for (int j = R; j > L; j--) {
+            (*Cf)++;
+            if (Arr[j] < Arr[j - 1]) {
+                int buffer = Arr[j];
+                Arr[j] = Arr[j - 1];
+                Arr[j - 1] = buffer;
+                k = j;
+                (*Mf) += 3;
+            }
+        }
+        L = k;
+
+        // Ð ÑŸÐ¡Ð‚Ð Ñ•Ð¡â€¦Ð Ñ•Ð Ò‘ Ð¡ÐƒÐ Â»Ð ÂµÐ Ð†Ð Â° Ð Ð…Ð Â°Ð Ñ—Ð¡Ð‚Ð Â°Ð Ð†Ð Ñ•
+        for (int j = L; j < R; j++) {
+            (*Cf)++;
+            if (Arr[j] > Arr[j + 1]) {
+                int buffer = Arr[j];
+                Arr[j] = Arr[j + 1];
+                Arr[j + 1] = buffer;
+                k = j;
+                (*Mf) += 3;
+            }
+        }
+        R = k;
+    } while (L < R);
+}
+
+void SelectSortSimple(int Arr[], int N, int *Cf, int *Mf){
+    *Cf = 0;
+    *Mf = 0;
+
+    for(int i = 0; i < N-1; i++){
+        int k = i;
+        for(int j = i; j < N; j++){
+            (*Cf)++;
+            if(Arr[j] < Arr[k]){
+                k = j;
+            }
+        }
+        if(1){ // k != i
+            int buffer = Arr[i];
+            Arr[i] = Arr[k];
+            Arr[k] = buffer;
+            (*Mf) += 2;
+        }
+    }
+}
+
 void BubbleSort(int Arr[], int N, int *Cf, int *Mf){
     *Cf = 0;
     *Mf = 0;
-    for(int i = 0; i < N; i++){
-        for(int j = N; j > i+1; j--){
+    for(int i = 0; i < N-1; i++){
+        for(int j = 0; j < N-i-1; j++){
             (*Cf)++;
-            if(Arr[j] <= Arr[j-1]){
+            if(Arr[j] > Arr[j+1]){
                 int buffer = Arr[j];
-                Arr[j] = Arr[j-1];
-                Arr[j-1] = buffer;
+                Arr[j] = Arr[j+1];
+                Arr[j+1] = buffer;
                 (*Mf) += 3;
             }
         }
@@ -92,21 +147,32 @@ int RunNumber(int Arr[], int N){
 }
 
 void PrintTableHeader() {
-    printf("| %-5s | %-33s | %-33s |\n", "N", "Bubble Sort", "Shaker Sort");
-    printf("|       | Убыв.   | Случ.   | Возр.         | Убыв.   | Случ.   | Возр.         |\n");
+    printf("| %-5s | %-13s | %-33s |\n", "N", " M + C ", "Insert Sort");
+    printf("|       |               | Убыв.   | Случ.   | Возр.         |\n");
 }
 
 void PrintTableRow(int N, int M_C_theoretical, int Mf_Cf_original[], int Mf_Cf_improved[]) {
-    printf("| %-5d | %-7d | %-7d | %-13d | %-7d | %-7d | %-13d |\n",
+    printf("| %-5d | %-13d | %-7d | %-7d | %-13d |\n",
            N,
-           Mf_Cf_original[0], Mf_Cf_original[1], Mf_Cf_original[2],
+           M_C_theoretical,
            Mf_Cf_improved[0], Mf_Cf_improved[1], Mf_Cf_improved[2]);
+}
+
+void PrintSecTableHeader() {
+    printf("| %-5s | %-13s | %-13s | %-13s | %-13s |\n", "N", " Select Sort ", " Bubble Sort ", " Shaker Sort ", " InsertSort ");
+}
+
+void PrintSecTableRow(int N, int M_C_Select, int M_C_Bubble, int M_C_Shaker, int M_C_Insert) {
+    printf("| %-5d | %-13d | %-13d | %-13d | %-13d |\n",
+           N,
+           M_C_Select, M_C_Bubble, M_C_Shaker, M_C_Insert);
 }
 
 int main() {
     int sumdo, sdo, sumposle, sposle;
-    int sizes[] = {100, 200, 300, 400, 500};
-    int Cf, Mf;
+    const int sizes[] = {100, 200, 300, 400, 500};
+    int Cf = 0, Mf = 0;
+    
     printf("\n");
     PrintTableHeader();
 
@@ -116,23 +182,8 @@ int main() {
         int Mf_Cf_original[3] = {0};
         int Mf_Cf_improved[3] = {0};
 
-        // Theoretical M+C for SelectSort is approximately N^2
-        int M_C_theoretical = (((N*N)-N)/2)+(3*(N-1));
-
-        // BubbleSort
-        FillInc(ArrInc, N);
-        BubbleSort(ArrInc, N, &Cf, &Mf);
-        Mf_Cf_original[2] = Mf + Cf;
-
-        FillDec(ArrDec, N);
-        BubbleSort(ArrDec, N, &Cf, &Mf);
-        Mf_Cf_original[0] = Mf + Cf;
-
-        FillRand(ArrRand, N);
-        BubbleSort(ArrRand, N, &Cf, &Mf);
-        Mf_Cf_original[1] = Mf + Cf;
-
-        // Improved SelectSort
+        int M_C_theoretical = (((N*N)-N)/2)+((((N*N)-N)/2)) + 2 * N - 2;
+        
         FillInc(ArrInc, N);
         InsertSort(ArrInc, N, &Cf, &Mf);
         Mf_Cf_improved[2] = Mf + Cf;
@@ -147,9 +198,44 @@ int main() {
         InsertSort(ArrRand, N, &Cf, &Mf);
         sumposle = CheckSum(ArrRand, N);
         sposle = RunNumber(ArrRand, N);
+        
+        if (sumdo != sumposle) {
+            printf("Warning: Checksum mismatch for N=%d\n", N);
+        }
+        
         Mf_Cf_improved[1] = Mf + Cf;
+        PrintTableRow(N, M_C_theoretical, 0, Mf_Cf_improved);
+    }
+    
+    printf("\n\n");
+    PrintSecTableHeader();
+    
+    for(int i = 0; i < 5; i++){
+        int N = sizes[i];
+        int ArrRand[N];
+        int Mf_Cf_Select = 0;
+        int Mf_Cf_Bubble = 0;
+        int Mf_Cf_Shaker = 0;
+        int Mf_Cf_Insert = 0;
 
-        PrintTableRow(N, M_C_theoretical, Mf_Cf_original, Mf_Cf_improved);
+        FillRand(ArrRand, N);
+        SelectSortSimple(ArrRand, N, &Cf, &Mf);
+        Mf_Cf_Select = Mf + Cf;
+
+        FillRand(ArrRand, N);
+        BubbleSort(ArrRand, N, &Cf, &Mf);
+        Mf_Cf_Bubble = Mf + Cf;
+
+        FillRand(ArrRand, N);
+        ShakerSort(ArrRand, N, &Cf, &Mf);
+        Mf_Cf_Shaker = Mf + Cf;
+
+        FillRand(ArrRand, N);
+        InsertSort(ArrRand, N, &Cf, &Mf);
+        Mf_Cf_Insert = Mf + Cf;
+
+        PrintSecTableRow(N, Mf_Cf_Select, Mf_Cf_Bubble, Mf_Cf_Shaker, Mf_Cf_Insert);
     }
     return 0;
 }
+
